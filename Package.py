66c8eecd951bd1af68453badcabf4a7d1cbe3619
RickyBui <buiricky1996@gmail.com>
2024-05-14 08:31:00 -0500

    Update Package.py

--- Package.py
+++ Package.py
@@ -1,7 +1,7 @@
 # Create class for packages
 
 class Packages:
-    def __init__(self, ID, address, city, state, zipcode, Deadline_time, weight, currentstatus, status, departuretime, deliverytime):
+    def __init__(self, ID, address, city, state, zipcode, Deadline_time, weight, status, departuretime, deliverytime):
         self.ID = ID
         self.address = address
         self.city = city
@@ -9,13 +9,12 @@ class Packages:
         self.zipcode = zipcode
         self.Deadline_time = Deadline_time
         self.weight = weight
-        self.currentstatus = currentstatus
-        self.status = status
+        self.status = "At the hub"
         self.departure_time = None # Departure time
         self.delivery_time = None # Delivery time
 
     def __str__(self):
-        return "ID:%s, %s, %s, %s, %s, %s, %s, %s, %s, Delivered at: %s" % (self.ID, self.address, self.city, self.state, self.zipcode,
+        return "ID:%s, %s, %s, %s, %s, %s, %s, %s, Departured time: %s, Delivered at: %s" % (self.ID, self.address, self.city, self.state, self.zipcode,
                                                        self.Deadline_time, self.weight, self.status, self.departure_time, self.delivery_time,)
 
     #def update_status(self, convert_timedelta):
