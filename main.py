f9ab4621e0fe9b3269cda418f85d8c1e5d7a80a0
RickyBui <buiricky1996@gmail.com>
2024-05-14 08:29:14 -0500

    main.py
    Fixed for package #9

--- main.py
+++ main.py
@@ -44,14 +44,12 @@ def load_package_data(filename, package_hash_table):
             pStatus = "At Hub"
             pDepartureTime = None
             pDeliveryTime = None
-            pCurrentStatus = None
 
             # Package object
-            p = Packages(pID, pAddress, pCity, pState, pZipcode, pDeadline_time, pWeight, pStatus, pDepartureTime, pDeliveryTime, pCurrentStatus)
+            p = Packages(pID, pAddress, pCity, pState, pZipcode, pDeadline_time, pWeight, pStatus, pDepartureTime, pDeliveryTime)
 
             # Insert data into hash table
             package_hash_table.insert(pID, p)
-
 
 
 # Finding distance between two address
@@ -71,17 +69,17 @@ def extract_address(address):
 
 # Manually load trucks
 # Truck 1 #13, 14, 15, 16,19, and 20 must all be together
-truck1 = Truck.Truck(16, 18, None, [1, 2, 4, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 0.0, "4001 South 700 East",
+truck1 = Truck.Truck(1, 18, None, [1, 2, 4, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 0.0, "4001 South 700 East",
                      datetime.timedelta(hours=8))
 
 # Truck 2 #3, 18, 36,and 38 must be on truck 2
-truck2 = Truck.Truck(16, 18, None, [3, 6, 12, 17, 18, 21, 23, 24, 25, 26, 27, 32, 33, 36, 38], 0.0,
-                     "4001 South 700 East", datetime.timedelta(hours=9, minutes=0))
+truck2 = Truck.Truck(2, 18, None, [3, 6, 12, 17, 18, 21, 23, 24, 25, 26, 27, 32, 33, 36, 38], 0.0,
+                     "4001 South 700 East", datetime.timedelta(hours=9, minutes=6))
 
 # Truck 3 #6, 25, 28, and 32 cannot leave before 9:05am
-truck3 = Truck.Truck(16, 18, None, [5, 7, 8, 9, 10, 11, 22, 28, 35, 39], 0.0, "4001 South 700 East",
-                     datetime.timedelta(hours=11, minutes=0))
-
+truck3 = Truck.Truck(3, 18, None, [5, 7, 8, 9, 10, 11, 22, 28, 35, 39], 0.0, "4001 South 700 East",
+                     datetime.timedelta(hours=10, minutes=21))
+#truck3.depart_time = min(truck1.time, truck2.time)
 # Create hash table
 package_hash_table = CreateHashMap()
 #print(package_hash_table.list_all())
@@ -120,20 +118,27 @@ def delivering_packages(truck):
         truck.time += datetime.timedelta(hours=next_address / 18)
         next_package.delivery_time = truck.time
         next_package.departure_time = truck.depart_time
-
+        next_package.truckNumber = truck.truckNumber
+        #if truck3.time >= datetime.timedelta(hours=10, minutes=20):
+            #package = package_hash_table.lookup(int(9))
+            #package.address = "410 S State S"
+            #package.zipcode = "84111"
+            #package_hash_table.insert(int(9), package)
 
 # Put the trucks through the loading process
 delivering_packages(truck1)
 delivering_packages(truck2)
 # Truck 3 will not leave until either truck 1 or truck 2 is finished delivering their packages
-truck3.depart_time = min(truck1.time, truck2.time)
+truck3.depart_time = datetime.timedelta(hours=10, minutes=21)
 delivering_packages(truck3)
-
 
 class Main:
     # User Interface
     # Below is used to check the status of the package at a certain time or of all the packages
     print("Western Governors University Parcel Service (WGUPS)")
+    print(f"Truck1: " f"{truck1.mileage}" " milage")
+    print(f"Truck2: " f"{truck2.mileage}" " milage")
+    print(f"Truck3: " f"{truck3.mileage}" " milage")
     # Print total mileage of all the trucks combine
     print(f"The total milage for Western Governors University Parcel Service (WGUPS)   {truck1.mileage + truck2.mileage + truck3.mileage}")
     # To check the status of of the packages the user will be asked to input "status"
@@ -146,6 +151,15 @@ class Main:
                 "Please enter a time to see the status of each package. Format: HH:MM. ")
             (h, m) = userTime.split(":")
             timeChange = datetime.timedelta(hours=int(h), minutes=int(m))
+            userTime = datetime.datetime.strptime(userTime, "%H:%M")
+            truck3.depart_time = datetime.datetime.strptime("10:21", "%H:%M")
+            if userTime >= truck3.depart_time:
+                package = package_hash_table.lookup(int(9))
+                package.address = "410 S State S"
+                package.zipcode = "84111"
+                package_hash_table.insert(int(9), package)
+            #if current_time > datetime.timedelta(hours=10, minutes=20):
+                #package_hash_table.list_all(40,'"ID:9, 300 State St, Salt Lake City, UT, 84103, EOD, 2 Kilos, En Route, Departured time: 10:21:00, Delivered at: 11:00:20 By Truck 3"')
             # The user will be prompt to check the status of a single package or all the packages
             second_input = input("To view the status of an individual package please type 'one'. For a rundown of all"
                                  " packages please type 'all'. ")
@@ -156,7 +170,7 @@ class Main:
                     solo_input = input("Enter the numeric package ID ")
                     package = package_hash_table.lookup(int(solo_input))
                     package.update_status(timeChange)
-                    print(str(package))
+                    print(str(package) + " By Truck " + str(package.truckNumber))
                 except ValueError:
                     print("Invalid Entry. Closing program.")
                     exit()
@@ -166,7 +180,7 @@ class Main:
                     for packageID in range(1, 41):
                         package = package_hash_table.lookup(packageID)
                         package.update_status(timeChange)
-                        print(str(package))
+                        print(str(package) +" By Truck " + str(package.truckNumber))
                 except ValueError:
                     print("Invalid entry. Closing program.")
                     exit()
@@ -178,3 +192,12 @@ class Main:
     elif input != " ":
         print("Invalid entry. Closing program.")
         exit()
+#myHash = package_hash_table
+#print(myHash.lookup(9))
+#myHash.insert(9, "410 S State St")
+#print(truck3.depart_time)
+#print(userTime)
+#package_hash_table.insert(9,"410 S State St")
+#print(type(timeChange())
+#myHash.list_all()
+#print(package_hash_table(9))
